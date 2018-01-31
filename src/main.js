a3f5ee500e51a1c5f7de0a2f547266841fc85ae5
Herbie <843834089@qq.com>
2018-01-31 09:10:16 +0800

    nint ui 轮播

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
@@ -10,11 +12,12 @@ import store from './vuex/store'
 import "./components/style.css"
 import "./components/app.js"
 
+
 // Vue.config.productionTip = false
 // Vue.config.silent = true
 
 /* eslint-disable no-new */
-
+Vue.use(MintUI)
 Vue.use(Vuex)
 
 new Vue({
